43dcf3aac97e0f2dfd1882314a14a4a8b1c1f470
LAGHZALI Taha <mrtahalz@gmail.com>
2016-03-01 16:29:55 +0100

    master

---
+++ src/main/java/com/dena/SuperCvApplication.java
@@ -0,0 +1,12 @@
+package com.dena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCvApplication.class, args);
+	}
+}
